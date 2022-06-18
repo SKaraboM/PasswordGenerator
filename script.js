9d9f6154c30beb4d9b18de1aaab54cb060f1d6ba
Karabo <makgobask2002@gmail.com>
2022-06-18 18:45:57 +0200

    Styling and javascript update

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
+const characters =["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
 "/"];
 let passwordEl1 = document.getElementById("password-el1")
 let passwordEl2 = document.getElementById("password-el2")
@@ -9,11 +9,11 @@ function generatePassword(){
     let passwordString2 = "";
     
     for(let i = 0; i < 15; i++){
-        let rand = Math.floor(Math.random() * characters.length)
-        let rand2 = Math.floor(Math.random() * characters.length)
-        passwordString1 += characters[rand];
-        passwordString2 += characters[rand2];
+
+        passwordString1 += characters[Math.floor(Math.random() * characters.length)];
+        passwordString2 += characters[Math.floor(Math.random() * characters.length)];
     }
+
     passwordEl1.innerHTML = passwordString1;
     passwordEl2.innerHTML = passwordString2;
 }
